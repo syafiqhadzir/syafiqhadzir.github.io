31f7e344c1c6f9408088fad1e23125b6bda21254
syafiqhadzir <syafiqhadzir@live.com.my>
2022-10-14 11:23:31 +0800

    Install and configure @nuxtjs/pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    buildModules: [
+        '@nuxtjs/pwa',
+    ],
+    pwa: {
+        icon: false // disables the icon module
+    }
 })
