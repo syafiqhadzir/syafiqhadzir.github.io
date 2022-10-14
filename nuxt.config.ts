cb72d97cab3a6c82c9dfa263a459b385ac21889f
syafiqhadzir <syafiqhadzir@live.com.my>
2022-10-14 11:42:22 +0800

    Install @nuxt/typescript-build @nuxt/types

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     buildModules: [
-        '@nuxtjs/pwa',
+        '@nuxtjs/pwa', '@nuxt/typescript-build'
     ],
     pwa: {
         icon: false // disables the icon module
