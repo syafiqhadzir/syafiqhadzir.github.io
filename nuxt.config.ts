c06451d17c1c258c78b3aa9d6eac0a35e07d0bfe
syafiqhadzir <syafiqhadzir@live.com.my>
2022-10-17 17:26:16 +0800

    Configure ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ["@nuxt/content"],
-    buildModules: [
-        '@nuxtjs/pwa', '@nuxt/typescript-build'
-    ],
-    pwa: {
-        icon: false // disables the icon module
-    },
-    "meta": [
-        {
-            "name": "viewport",
-            "content": "width=device-width, initial-scale=1"
-        },
-        {
-            "charset": "utf-8"
-        }
-    ],
-    noscript: [
-        // <noscript>Javascript is required</noscript>
-        { children: 'Javascript is required' }
-    ],
-    content: {
-        // https://content.nuxtjs.org/api/configuration
-    }
+  modules: ['@nuxt/content'],
+  buildModules: [
+    '@nuxtjs/pwa', '@nuxt/typescript-build'
+  ],
+  pwa: {
+    icon: false // disables the icon module
+  },
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  }
 })
